5dba96a484678760756bf6580bef39cdf7ea06db
ben <me@benhunt.io>
2025-06-19 18:21:47 -0500

    change font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Typography, Box } from '@mui/joy';
 export default function Home() {
   return (
     <Box sx={{ p: 4, maxWidth: 800, mx: 'auto' }}>
-      <Typography level="h1" sx={{ fontSize: '10rem' }}>
+      <Typography level="h1" sx={{ fontSize: '5rem' }}>
         I ❤️ Brittany Pollok
       </Typography>
     </Box>
