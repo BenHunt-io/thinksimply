58ed37ad436b38e4be3bb46a4c4adca2be09d6d7
ben <me@benhunt.io>
2025-06-21 11:13:52 -0500

    get eslint / prettier working and format imports on save

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import { Providers } from './providers';
 
 
 export default function RootLayout({
@@ -9,9 +8,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Providers>
           {children}
-        </Providers>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
-import { Typography, Box } from '@mui/joy';
+import { Box } from "@mui/joy";
+
+const Header = () => {
+  return (
+    <Box
+      sx={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        right: 0,
+        width: "100%",
+        backgroundColor: "#171717",
+        color: "white",
+        padding: "1rem 2rem",
+      }}
+    ></Box>
+  );
+};
 
 export default function Home() {
-  return (
-    <Box sx={{ p: 4, maxWidth: 800, mx: 'auto' }}>
-      <Typography level="h1" sx={{ fontSize: '5rem' }}>
-        I ❤️ Brittany Pollok
-      </Typography>
-    </Box>
-  );
+  return <Header />;
 }
