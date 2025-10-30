e5af0a5cc7565fd35eb03b641bb4b271ab6f04a7
ben <me@benhunt.io>
2025-10-30 09:05:26 -0500

    refactor: test invalidating cache every 10 sec

--- src/app/posts/[post]/page.tsx
+++ src/app/posts/[post]/page.tsx
@@ -1,4 +1,3 @@
-import Post from "@/app/components/Post.tsx";
 import { postSchema } from "@/app/posts/postSchema.ts";
 import { Typography } from "@mui/joy";
 import fs from "fs";
@@ -7,7 +6,7 @@ import matter from "gray-matter";
 const postsRelativePath = `./src/posts`;
 
 // Next.js will invalidate the cache when a request comes in, at most once every 60 seconds.
-export const revalidate = 60;
+export const revalidate = 10;
 
 export default async function PostPage({
   params,
@@ -15,32 +14,36 @@ export default async function PostPage({
   params: Promise<{ post: string }>;
 }) {
   const { post } = await params;
-  const { metadata, content } = await readMarkdownFile(post);
+  // const { metadata, content } = await readMarkdownFile(post);
 
   return (
     <>
       <Typography level="h1">{new Date().toISOString()}</Typography>
-      <Post markdownContent={content} title={metadata.title} />
+      {/* <Post markdownContent={content} title={metadata.title} /> */}
     </>
   );
 }
 
-export async function generateStaticParams() {
-  const fileNames = await fs.promises.readdir(postsRelativePath);
-
-  const params = [];
-  for (const fileName of fileNames) {
-    const fileContent = await fs.promises.readFile(
-      `${postsRelativePath}/${fileName}`,
-      "utf8",
-    );
-    const { data } = matter(fileContent);
-    const parsedData = postSchema.parse(data);
-    params.push({ post: parsedData.pathSegment });
-  }
-
-  return params;
+export const generateStaticParams = async () => {
+  return [{ post: "who-are-you" }, { post: "in-defense-of-crypto" }];
 }
+
+// export async function generateStaticParams() {
+//   const fileNames = await fs.promises.readdir(postsRelativePath);
+
+//   const params = [];
+//   for (const fileName of fileNames) {
+//     const fileContent = await fs.promises.readFile(
+//       `${postsRelativePath}/${fileName}`,
+//       "utf8",
+//     );
+//     const { data } = matter(fileContent);
+//     const parsedData = postSchema.parse(data);
+//     params.push({ post: parsedData.pathSegment });
+//   }
+
+//   return params;
+// }
 
 // Need to figure out how to render the markdown without messing up the header
 async function readMarkdownFile(postPathSegment: string) {
