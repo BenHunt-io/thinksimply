6576eb085f10788077a8a768d644cd4785a22d02
ben <me@benhunt.io>
2025-10-30 08:34:42 -0500

    refactor: test invalidating the cache

--- src/app/posts/[post]/page.tsx
+++ src/app/posts/[post]/page.tsx
@@ -1,10 +1,13 @@
 import Post from "@/app/components/Post.tsx";
 import { postSchema } from "@/app/posts/postSchema.ts";
+import { Typography } from "@mui/joy";
 import fs from "fs";
 import matter from "gray-matter";
 
 const postsRelativePath = `./src/posts`;
 
+// Next.js will invalidate the cache when a request comes in, at most once every 60 seconds.
+export const revalidate = 60;
 
 export default async function PostPage({
   params,
@@ -15,26 +18,29 @@ export default async function PostPage({
   const { metadata, content } = await readMarkdownFile(post);
 
   return (
-    <Post markdownContent={content} title={metadata.title} />
+    <>
+      <Typography level="h1">{process.env.RUNTIME_VARIABLE || "No runtime variable"}</Typography>
+      <Post markdownContent={content} title={metadata.title} />
+    </>
   );
 }
 
-// export async function generateStaticParams() {
-//   const fileNames = await fs.promises.readdir(postsRelativePath);
+export async function generateStaticParams() {
+  const fileNames = await fs.promises.readdir(postsRelativePath);
 
-//   const params = [];
-//   for (const fileName of fileNames) {
-//     const fileContent = await fs.promises.readFile(
-//       `${postsRelativePath}/${fileName}`,
-//       "utf8",
-//     );
-//     const { data } = matter(fileContent);
-//     const parsedData = postSchema.parse(data);
-//     params.push({ post: parsedData.pathSegment });
-//   }
+  const params = [];
+  for (const fileName of fileNames) {
+    const fileContent = await fs.promises.readFile(
+      `${postsRelativePath}/${fileName}`,
+      "utf8",
+    );
+    const { data } = matter(fileContent);
+    const parsedData = postSchema.parse(data);
+    params.push({ post: parsedData.pathSegment });
+  }
 
-//   return params;
-// }
+  return params;
+}
 
 // Need to figure out how to render the markdown without messing up the header
 async function readMarkdownFile(postPathSegment: string) {
